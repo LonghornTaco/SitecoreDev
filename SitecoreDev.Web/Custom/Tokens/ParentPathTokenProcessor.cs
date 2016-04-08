b39b6affca110d350b8cef6f99c95901c319c751
Jason Wilkerson <jasonmwilkerson@gmail.com>
2016-04-08 00:23:45 -0500

    Adding CreditCard field, ParentPathTokenProcessor and SsnValidator.

---
+++ SitecoreDev.Web/Custom/Tokens/ParentPathTokenProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.ExpandInitialFieldValue;
+
+namespace SitecoreDev.Web.Custom.Tokens
+{
+   public class ParentPathTokenProcessor : ExpandInitialFieldValueProcessor
+   {
+      public override void Process(ExpandInitialFieldValueArgs args)
+      {
+         var token = args.SourceField.Value;
+         if (!String.IsNullOrEmpty(token) && token.Contains("$parentPath"))
+         {
+            if (args.TargetItem != null)
+            {
+               args.Result = args.Result.Replace("$parentPath", args.TargetItem.Paths.ParentPath);
+            }
+         }
+      }
+   }
+}
