b7d8cd79137de8993e86e0237f8209514abb36e8
Jason Wilkerson <jasonmwilkerson@gmail.com>
2016-06-15 23:47:02 -0500

    Updating HeroSlider to use Glass.Mapper for EE.

--- Feature/Media/SitecoreDev.Feature.Media/Controllers/MediaController.cs
+++ Feature/Media/SitecoreDev.Feature.Media/Controllers/MediaController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Glass.Mapper.Sc;
 using Sitecore.Data.Fields;
@@ -17,11 +19,13 @@ namespace SitecoreDev.Feature.Media.Controllers
    {
       private readonly IContextWrapper _contextWrapper;
       private readonly IMediaContentService _mediaContentService;
+      private readonly IGlassHtml _glassHtml;
 
-      public MediaController(IContextWrapper contextWrapper, IMediaContentService mediaContentService)
+      public MediaController(IContextWrapper contextWrapper, IMediaContentService mediaContentService, IGlassHtml glassHtml)
       {
          _contextWrapper = contextWrapper;
          _mediaContentService = mediaContentService;
+         _glassHtml = glassHtml;
       }
 
       public ViewResult HeroSlider()
@@ -35,9 +39,7 @@ namespace SitecoreDev.Feature.Media.Controllers
             {
                viewModel.HeroImages.Add(new HeroSliderImageViewModel()
                {
-                  Id = slide.Id.ToString(),
-                  MediaUrl = slide.Image?.Src,
-                  AltText = slide.Image?.Alt
+                  Image = new HtmlString(_glassHtml.Editable<IHeroSliderSlide>(slide, i => i.Image))
                });
             }
             var firstItem = viewModel.HeroImages.FirstOrDefault();
@@ -55,43 +57,5 @@ namespace SitecoreDev.Feature.Media.Controllers
 
          return View(viewModel);
       }
-
-      //public ViewResult OldHeroSlider()
-      //{
-      //   var viewModel = new HeroSliderViewModel();
-
-      //   if (!String.IsNullOrEmpty(RenderingContext.Current.Rendering.DataSource))
-      //   {
-      //      var contentItem = _repository.GetItem(RenderingContext.Current.Rendering.DataSource);
-      //      if (contentItem != null)
-      //      {
-      //         var heroImagesField = new MultilistField(contentItem.Fields["Hero Images"]);
-      //         if (heroImagesField != null)
-      //         {
-      //            var items = heroImagesField.GetItems();
-      //            var itemCounter = 0;
-      //            foreach (var item in items)
-      //            {
-      //               var mediaItem = (MediaItem)item;
-      //               viewModel.HeroImages.Add(new HeroSliderImageViewModel()
-      //               {
-      //                  MediaUrl = MediaManager.GetMediaUrl(mediaItem),
-      //                  AltText = mediaItem.Alt,
-      //                  IsActive = itemCounter == 0
-      //               });
-      //               itemCounter++;
-      //            }
-      //         }
-      //      }
-      //   }
-
-      //   var parameterValue = _contextWrapper.GetParameterValue("Slide Interval in Milliseconds");
-
-      //   int interval = 0;
-      //   if (int.TryParse(parameterValue, out interval))
-      //      viewModel.SlideInterval = interval;
-
-      //   return View(viewModel);
-      //}
    }
 }
